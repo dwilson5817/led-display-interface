7c53f7ef951cc62cf0d8ed6465e5d146413ee26b
Dylan Wilson <dwilson5817@gmail.com>
2022-05-29 13:14:35 +0200

    Fix issue where Spotify could only be connected on localhost
    Only on localhost will Spotipy start a web server to receive the auth
    code.  Use of the SpotifyPKCE class and the manual reading of the auth
    code fixes this error.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import os
 
-import flask
-import spotipy
 from dotenv import load_dotenv
-from flask import Flask, flash
-from flask import render_template, request, url_for, redirect
-from spotipy import SpotifyOAuth, SpotifyOauthError
+from flask import Flask, request, flash, redirect, url_for, render_template, jsonify
+from spotipy import CacheFileHandler, SpotifyPKCE, Spotify, SpotifyOauthError
 
-from utils.commands import run_cmd, is_up
+from utils.commands import is_up, run_cmd
 from utils.files import read_value, write_file
 
 app = Flask(__name__)
@@ -17,8 +14,9 @@ load_dotenv()
 app.secret_key = os.getenv('SESSION_KEY')
 app.config['SESSION_TYPE'] = 'filesystem'
 
-spotify_oauth = SpotifyOAuth(scope='user-read-currently-playing')
-spotify = spotipy.Spotify(client_credentials_manager=spotify_oauth)
+spotify_cache = CacheFileHandler()
+spotify_oauth = SpotifyPKCE(scope='user-read-currently-playing', open_browser=False, cache_handler=spotify_cache)
+spotify = Spotify(client_credentials_manager=spotify_oauth)
 
 message = read_value('message')
 
@@ -33,7 +31,7 @@ def index():
 
         if not text:
             flash('The message cannot be empty!', 'danger')
-            return redirect('/')
+            return redirect(url_for('index'))
         else:
             write_file(message=text)
             message = text
@@ -43,76 +41,105 @@ def index():
             else:
                 flash('The message was saved but the service is currently stopped!', 'warning')
 
-            return redirect('/')
+            return redirect(url_for('index'))
 
     if spotify_oauth.get_cached_token():
-        return render_template('index.html', message=message, is_up=is_up(), spotify=spotify)
+        return render_template('index.html', message=message, is_up=is_up(), current_user=spotify.current_user())
 
     return render_template('index.html', message=message, is_up=is_up())
 
 
-@app.route('/start', methods=['GET'])
-def start():
+@app.route('/service/start', methods=['GET'])
+def service_start():
     return run_cmd(['systemctl', 'start', 'led-display.service'],
                    'Successfully started systemd service!',
                    'Failed to start systemd service!')
 
 
-@app.route('/stop', methods=['GET'])
-def stop():
+@app.route('/service/stop', methods=['GET'])
+def service_stop():
     return run_cmd(['systemctl', 'stop', 'led-display.service'],
                    'Successfully stopped systemd service!',
                    'Failed to stop systemd service!')
 
 
-@app.route('/restart', methods=['GET'])
-def restart():
+@app.route('/service/restart', methods=['GET'])
+def service_restart():
     return run_cmd(['systemctl', 'restart', 'led-display.service'],
                    'Successfully restarted systemd service!',
                    'Failed to restart systemd service!')
 
 
 @app.route('/spotify/connect', methods=['GET'])
-def connect():
-    try:
-        if spotify:
-            display_name = spotify.current_user().get('display_name')
+def spotify_connect():
+    if _is_logged_in():
+        flash('An account is already connected!', 'warning')
+        return redirect(url_for('index'))
 
+    return redirect(spotify_oauth.get_authorize_url())
+
+
+@app.route('/spotify/save', methods=['GET'])
+def spotify_save():
+    if _is_logged_in():
+        flash('An account is already connected!', 'warning')
+        return redirect(url_for('index'))
+
+    try:
+        code = spotify_oauth.parse_response_code(request.url)
+
+        if not code:
+            return redirect(url_for('spotify_connect'))
+
+        access_token = spotify_oauth.get_access_token(code=code)
+        display_name = spotify.me().get('display_name')
+
+        if access_token:
             flash(
                 'Hello, <strong>{}</strong>!  You account is connected!'.format(display_name),
                 'success'
             )
+        else:
+            flash(
+                'Failed to connect to your Spotify account.',
+                'danger'
+            )
+
     except SpotifyOauthError as e:
         flash(
-            'Failed to connect to your Spotify account.  Got error: {}'.format(e),
+            'Failed to connect to your Spotify account.  {}'.format(e),
             'danger'
         )
 
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 @app.route('/spotify/disconnect', methods=['GET'])
-def disconnect():
+def spotify_disconnect():
     os.remove(".cache")
 
     flash('Your account was disconnected!', 'success')
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 @app.route('/api/message', methods=['GET'])
-def get_message():
-    return flask.jsonify(message=message)
+def api_get_message():
+    return jsonify(message=message)
 
 
 @app.route('/api/currently_playing', methods=['GET'])
-def currently_playing():
-    if spotify_oauth.get_cached_token():
+def api_currently_playing():
+    if _is_logged_in() and spotify.currently_playing():
         song = spotify.currently_playing().get('item').get('name')
         artists = [ artist['name'] for artist in spotify.currently_playing().get('item').get('album').get('artists') ]
 
-        return flask.jsonify(currently_playing='{} by {}'.format(song, ', '.join(artists)))
+        return jsonify(currently_playing='{} by {}'.format(song, ', '.join(artists)))
     else:
-        return flask.jsonify(currently_playing=False)
+        return jsonify(currently_playing=False)
+
+
+def _is_logged_in():
+    return spotify_cache.get_cached_token()
 
 
 if __name__ == '__main__':
--- utils/commands.py
+++ utils/commands.py
@@ -1,11 +1,15 @@
 import subprocess
+from os import getenv
 
 from flask import flash, redirect
 
 
 def is_up():
-    stat = subprocess.call(['systemctl', 'is-active', '--quiet', 'led-display'])
-    return stat == 0
+    if getenv('DEVELOPMENT', False):
+        return True
+    else:
+        stat = subprocess.call(['systemctl', 'is-active', '--quiet', 'led-display'])
+        return stat == 0
 
 
 def run_cmd(cmd, success_message, failure_message):
